9f38b59b6a80378d9e897a5bafe0583e02b5473f
AkbarGM <56382572+AkbarGM@users.noreply.github.com>
2020-04-15 12:45:04 +0500

    Add project files.

---
+++ DataExample/ProductsRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataExample
+{
+    class ProductsRepository
+    {
+        SqlConnect sqlConnect;
+        public ProductsRepository()
+        {
+            sqlConnect = new SqlConnect();
+        }
+        public void GetProducts()
+        {
+            string str = "SELECT * FROM Products";
+            sqlConnect.Open();
+            sqlConnect.Get(str);
+            sqlConnect.Close();
+        }
+        public void GetProductsById(int Id)
+        {
+            string str = $"SELECT * FROM Products WHERE ID={Id}";
+            sqlConnect.Open();
+            sqlConnect.Get(str);
+            sqlConnect.Close();
+        }
+        public void PutProducts(string name,decimal price)
+        {
+            string str = $"INSERT INTO Products VALUES ('{name}','{price}')";
+            sqlConnect.Open();
+            sqlConnect.Insert(str);
+            sqlConnect.Close();
+        }
+        public void DeleteProducts(int Id)
+        {
+            string str = $"DELETE Products WHERE ID={Id}";
+            sqlConnect.Open();
+            sqlConnect.Delete(str);
+            sqlConnect.Close();
+        }
+
+
+    }
+}
---
+++ DataExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductsRepository products = new ProductsRepository();
+            Console.WriteLine("Welcome to our store. Choose a command:\n1-ProductList\n2-Product\n3-Insert Product\n4-Delete Product");
+            l1:
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch(n)
+            {
+                case 1: products.GetProducts();
+                    break;
+                case 2:
+                    Console.Write("Input ID of Products: ");
+                    int Id = Convert.ToInt32(Console.ReadLine());
+                    products.GetProductsById(Id);
+                    break;
+                case 3:
+                    Console.Write("Name of Product:");
+                    string name = Console.ReadLine();
+                    Console.Write("Price of Product:");
+                    decimal price = Convert.ToDecimal(Console.ReadLine());
+                    products.PutProducts(name, price);
+                    break;
+
+            }
+            goto l1;
+            
+
+            Console.ReadLine();
+        }
+    }
+}
